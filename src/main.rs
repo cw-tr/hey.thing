f27cbaa3ca67ac1c1f697f3440977cd689cf203b
Mukan Erkin <mukanerkintoruk@gmail.com>
2026-03-13 09:14:27 +0300

    fix: remove non-portable manual_inspect test from main.rs

--- src/main.rs
+++ src/main.rs
@@ -56,14 +56,3 @@ fn main() {
     }
 }
 
-#[cfg(test)]
-mod manual_inspect {
-    use super::*;
-    #[test]
-    fn test_inspect_tree() {
-        let store = crate::storage::kv_store::KvStore::open(".something/db").unwrap();
-        let tree_hash = "56851dcff88db93a8247b86af9ca0371ec777d3c424ad9cee8481aea9cd8bf2b";
-        let data = store.get(tree_hash.as_bytes()).unwrap().unwrap();
-        println!("DATA_START:{}:DATA_END", String::from_utf8_lossy(&data));
-    }
-}
